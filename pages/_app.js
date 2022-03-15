b3168fb27cf7e4a6aef41d4b8c8d03173b4bb9be
Jung ChaeSang <che@Jungui-iMac.local>
2022-03-15 21:13:29 +0900

    first-commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import { GlobalStyle } from "../styles/global-style";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>nextjs try</title>
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
